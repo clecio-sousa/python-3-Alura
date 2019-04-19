84992b6be7d19540b6fe37e35bbd5cad7dc62254
clecio1201 <clecio1201@hotmail.com>
2019-04-18 23:52:33 -0300

    jogo da forca alterado. Adicao das funcoes strip()e upper().

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,6 +4,31 @@ def jogar_forca():
     print("BEM VINDO AO JOGO DA FORCA!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    letras_acertadas =["_","_","_","_","_","_"]
+
+    acertou = False
+    enforcou = False
+
+
+    # enquanto nao enforcou e nao acertou faça
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra...?")
+        chute = chute.strip()# retira espacos em branco
+        posicao = 0
+
+        for letra in palavra_secreta:
+
+            if (chute.upper()==letra.upper()):# coloca todas as letras em maiuscula
+                letras_acertadas[posicao] = letra
+                #print("Encontrei a letra {} na posição {}" .format(letra, index))
+            posicao += 1
+
+        print(letras_acertadas)
+        letras_faltando = str(letras_acertadas.count("_"))
+        print("Ainda faltam {} letras".format(letras_faltando))
+
     print("FIM DE JOGO")
 
 if(__name__ == "__main__"):
